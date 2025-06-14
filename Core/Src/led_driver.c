9a04e6e8b9abc35b2121eb377d87a566f4db708d
Johan Sebastian Mendieta Dilbert <jomendietad@unal.edu.co>
2025-06-13 21:05:33 -0500

    Se agrega la documentación del LED driver

--- Core/Src/led_driver.c
+++ Core/Src/led_driver.c
@@ -1,17 +1,31 @@
 #include "led_driver.h"
-
+/**
+ * @brief LED driver initialization.
+ */
 void led_init(led_handle_t *led) {
     HAL_GPIO_WritePin(led->port, led->pin, GPIO_PIN_RESET);
 }
 
+/**
+ * @brief Turn on the LED.
+ * @param led Pointer to the LED handle.
+ */
 void led_on(led_handle_t *led) {
     HAL_GPIO_WritePin(led->port, led->pin, GPIO_PIN_SET);
 }
 
+/**
+ * @brief Turn off the LED.
+ * @param led Pointer to the LED handle.
+ */
 void led_off(led_handle_t *led) {
     HAL_GPIO_WritePin(led->port, led->pin, GPIO_PIN_RESET);
 }
 
+/**
+ * @brief Toggle the LED state.
+ * @param led Pointer to the LED handle.
+ */
 void led_toggle(led_handle_t *led) {
     HAL_GPIO_TogglePin(led->port, led->pin);
 }
